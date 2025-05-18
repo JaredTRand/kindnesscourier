071e25e3a8007f0ba22d4603d8982aaa93c034ab
Jared Rand <31776017+JaredTRand@users.noreply.github.com>
2025-05-17 23:48:25 -0700

    goodbye old controller, hellow new controller 👋

--- addons/playercontroller/Levels/OutOfBounds.gd
+++
@@ -1,6 +0,0 @@
-extends Area3D
-
-# When the player enters the world boundary restart the level.
-func _on_body_entered(body: Node3D) -> void:
-	if body is PhantomPlayer:
-		UserInterface.restart_current_scene()
