9a8461fe73cb227e3aa77823a93d8eb3e108e476
Jared Rand <31776017+JaredTRand@users.noreply.github.com>
2025-05-28 21:42:20 -0700

    oh oops i forgot to commit in a while

--- World/NPCs/npc.gd
+++ World/NPCs/npc.gd
@@ -1,15 +1,36 @@
 extends CharacterBody3D
 
 @export var npc_name: String = ""
+@export var animations:AnimationPlayer
+@export var dialogue:DialogicTimeline 
 
 @onready var npc_pcam:PhantomCamera3D = %NPCPhantomCamera3D
-@onready var interact_move_to:Vector3 = %MoveToLocation.get_global_position()
-@onready var interact_rotate_to:Vector3 = %MoveToLocation.get_global_rotation()
-@onready var interact_dialogue: Sprite3D = %InteractDialogue
+@onready var interact_move_to:Vector3
+@onready var interact_rotate_to:Vector3 
+@onready var interact_dialogue:Sprite3D = %InteractDialogue
+
+#const my_scene: PackedScene = preload("res://World/interact_dialogue.tscn")
 #@onready var is_interacting:bool = false
 
 func _ready() -> void:
 	add_to_group("npc")
+	
+	var anim : Animation= animations.get_animation("Idle")
+	anim.loop_mode = Animation.LOOP_PINGPONG
+	animations.play(anim.resource_name)
+	
+	
+	if %MoveToLocation: 
+		interact_move_to = %MoveToLocation.get_global_position()
+		interact_rotate_to = %MoveToLocation.get_global_rotation()
+	#else:
+		#var tempnode:Node3D = Node3D.new()
+		#tempnode.translate_object_local(Vector3(0,0,2))
+		#interact_move_to   = tempnode.get_global_position()
+		#interact_rotate_to = tempnode.get_global_rotation()
+		
+	#if !interact_dialogue: 
+		#interact_dialogue = my_scene.instantiate()
 
 func do_colliding():
 	interact_dialogue.show()
@@ -18,11 +39,11 @@ func stop_colliding():
 
 func do_interact():
 		# check if a dialog is already running 
-	if Dialogic.current_timeline != null:
+	if Dialogic.current_timeline != null || !dialogue:
 		return
 	
 	#is_interacting = true
-	Dialogic.start("res://World/NPCs/npc1/dialogue/timeline1.dtl")
+	Dialogic.start(dialogue)
 	get_viewport().set_input_as_handled()
 func stop_interact():
 	pass
