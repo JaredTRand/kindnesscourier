769f685ccec267e6f032472934dcc1afd050c5cb
Jared Rand <31776017+JaredTRand@users.noreply.github.com>
2025-05-29 23:52:32 -0700

    fuck the pics man lol

--- World/NPCs/npc.gd
+++ World/NPCs/npc.gd
@@ -1,13 +1,14 @@
 extends CharacterBody3D
 
 @export var npc_name: String = ""
+@export var npc_pic:CompressedTexture2D
 @export var animations:AnimationPlayer
 @export var dialogue:DialogicTimeline 
 @export var dialogue_char:DialogicCharacter
 
 
-@onready var npc_pcam:PhantomCamera3D = %NPCPhantomCamera3D
-@onready var pic_view:SubViewport = %pic_view
+#@onready var pic_view:SubViewport = %pic_view
+#@onready var pic_cam:Camera3D = %piccam
 @onready var interact_move_to:Vector3
 @onready var interact_rotate_to:Vector3 
 @onready var interact_dialogue:Sprite3D = %InteractDialogue
@@ -22,10 +23,15 @@ func _ready() -> void:
 	var anim : Animation= animations.get_animation("Idle")
 	anim.loop_mode = Animation.LOOP_PINGPONG
 	animations.play(anim.resource_name)
-	
+	#
 	if %MoveToLocation: 
 		interact_move_to = %MoveToLocation.get_global_position()
 		interact_rotate_to = %MoveToLocation.get_global_rotation()
+		#pic_cam.global_position = %MoveToLocation.get_global_position()
+		#pic_cam.global_rotation = %MoveToLocation.get_global_rotation()
+		#pic_cam.position = Vector3(pic_cam.position.x, pic_cam.position.y+2, pic_cam.position.z+2)
+		#pic_cam.rotate_y(-90)
+		
 	#else:
 		#var tempnode:Node3D = Node3D.new()
 		#tempnode.translate_object_local(Vector3(0,0,2))
@@ -34,6 +40,7 @@ func _ready() -> void:
 		
 	#if !interact_dialogue: 
 		#interact_dialogue = my_scene.instantiate()
+	dialogue_char.display_name = npc_name
 
 func do_colliding():
 	interact_dialogue.show()
@@ -44,11 +51,11 @@ func do_interact():
 		# check if a dialog is already running 
 	var filename = "res://World/NPCs/assets/profpics/npcpic-" + npc_name + ".png"
 	
-	pic_view.render_target_update_mode = pic_view.UPDATE_ONCE
-	await RenderingServer.frame_post_draw
-	
-	get_viewport().get_texture().get_image().save_png(filename)
-	dialogue_char.portraits["neutral"].export_overrides.image = filename
+	#pic_view.render_target_update_mode = SubViewport.UPDATE_ONCE
+	#await RenderingServer.frame_post_draw
+	##
+	#pic_view.get_texture().get_image().save_png(filename)
+	#dialogue_char.portraits["neutral"]  = filename
 	
 	if Dialogic.current_timeline != null || !dialogue:
 		return
